f13c74d4fe4dd451cfccdd290b239880f9044d93
shubham <shubham.pandey128@gmail.com>
2020-02-23 17:25:02 +0530

    Feb changes

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,16 @@
-/*! Arrowmet v0.0.1 description-IT Service Company | (c) 2019 Arrowmet http://arrowmet.com/ */
+/*! Arrowmet v0.0.1 description-IT Service Company | (c) 2020 Arrowmet http://arrowmet.com/ */
+$((function() {
+  $(document).scroll((function() {
+    var $nav = $("#header");
+    var $logoText = $("#logoText");
+    var $logoSvg = $("#logoSvg");
+    $nav.toggleClass("fixed", $(this).scrollTop() > $nav.height());
+    if ($(this).scrollTop() > $nav.height()) {
+      $logoText.css("fill", "#4F4F4F");
+      $logoSvg.css("transform", "translateX(0px)");
+    } else {
+      $logoText.css("fill", "#fff");
+      $logoSvg.css("transform", "translateX(-30px)");
+    }
+  }));
+}));
